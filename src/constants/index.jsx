2f272168c279c8d7e3a4d2d1d6a725600b479341
harsh6468 <harsh.bhumca21@gmail.com>
2025-10-12 18:17:35 +0530

    Added Disease pridection project

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -15,12 +15,12 @@ export const PROFILE = {
         { name: "Projects", href: "#projects" },
         { name: "Resume", href: "/resume.pdf" },
     ],
-    social_media_links:{
+    social_media_links: {
         LinkedIn: { url: "https://www.linkedin.com/in/harsh-9119422a8/", icon: "fa fa-linkedin" },
         GitHub: { url: "https://github.com/Harsh6468", icon: "fa fa-github" },
         Resume: { url: "/resume.pdf", icon: "fa fa-file" },
     },
-    emailjs_cred:{
+    emailjs_cred: {
         SERVICE_ID: import.meta.env.VITE_SERVICE_ID,
         TEMPLATE_ID: import.meta.env.VITE_TEMPLATE_ID,
         PUBLIC_KEY: import.meta.env.VITE_PUBLIC_KEY,
@@ -50,6 +50,14 @@ export const PROFILE = {
     ],
     projects: [
         {
+            title: "Disease Prediction System",
+            desc:
+                "Built a machine learning-based disease prediction system that predicts possible diseases based on user-reported symptoms. Implemented comprehensive data preprocessing, feature encoding, and model training using Scikit-learn. Developed a React frontend for user interaction and a FastAPI backend for serving predictions in real time. Deployed the full-stack application for public access.",
+            tech: ["Python", "Pandas", "NumPy", "Scikit-learn", "FastAPI", "React", "Tailwind CSS", "Vercel", "MongoDB"],
+            link: "https://disease-prediction-mu.vercel.app/",
+            repo: "https://github.com/Harsh6468/disease-prediction",
+        },
+        {
             title: "Waste Management Efficiency Prediction",
             desc:
                 "Built and deployed a machine learning model to predict waste management efficiency. Implemented data preprocessing pipeline with encoding, scaling, and model persistence using Joblib. Developed a Flask REST API for serving predictions and deployed it on Render Cloud. Optimized performance with cross-validation and hyperparameter tuning for higher accuracy.",
@@ -60,7 +68,7 @@ export const PROFILE = {
         {
             title: "Climate Hazard Trend Analyzer ",
             desc:
-            "Built a tool to analyze and visualize heatwave trends by region using real-world climate data. Designed interactive charts to highlight high-temperature zones over time. Integrated data filters for users to view patterns by city, state, or custom coordinates.",
+                "Built a tool to analyze and visualize heatwave trends by region using real-world climate data. Designed interactive charts to highlight high-temperature zones over time. Integrated data filters for users to view patterns by city, state, or custom coordinates.",
             tech: ["Python", "Pandas", "NumPy", "FastAPI", "React", "Tailwind CSS", "Vercel",],
             link: "https://climate-hazard-analyzer.vercel.app/",
             repo: "https://github.com/Harsh6468/climate-hazard-analyzer",
@@ -68,7 +76,7 @@ export const PROFILE = {
         {
             title: "Real-Time Chat Application",
             desc:
-            "A React-based chat application built using the MERN stack (MongoDB, Express.js, React, Node.js). Users can create an account, log in, and chat in real time with other users. The app includes online/offline status indicators, theme customization (light/dark mode), and a responsive user interface for a seamless messaging experience.",
+                "A React-based chat application built using the MERN stack (MongoDB, Express.js, React, Node.js). Users can create an account, log in, and chat in real time with other users. The app includes online/offline status indicators, theme customization (light/dark mode), and a responsive user interface for a seamless messaging experience.",
             tech: ["React", "Tailwind CSS", "Nodejs", "Express", "Mongodb", "Render"],
             link: "https://climate-hazard-analyzer.vercel.app/",
             repo: "https://github.com/Harsh6468/chat-app",
@@ -76,7 +84,7 @@ export const PROFILE = {
         {
             title: "RBAC-UI",
             desc:
-            "React Role Management App – Built a role-based access control system where admins can assign and manage user roles with a responsive React UI.",
+                "React Role Management App – Built a role-based access control system where admins can assign and manage user roles with a responsive React UI.",
             tech: ["React", "Tailwind CSS", "vercel"],
             link: "https://rbac-ui-one.vercel.app/",
             repo: "https://github.com/Harsh6468/rbac-ui",
@@ -84,7 +92,7 @@ export const PROFILE = {
         {
             title: "EV-Dashboard",
             desc:
-            "React EV Dashboard – Developed a dashboard in React that reads data from CSV files and generates interactive charts for visualizing EV-related metrics.",
+                "React EV Dashboard – Developed a dashboard in React that reads data from CSV files and generates interactive charts for visualizing EV-related metrics.",
             tech: ["React", "Tailwind CSS", "Chart.js", "vercel"],
             link: "https://ev-dashboard-gilt.vercel.app/",
             repo: "https://github.com/Harsh6468/ev-dashboard",
@@ -92,7 +100,7 @@ export const PROFILE = {
         {
             title: "Swan Public School",
             desc:
-            "Responsive Practice Website – Built a static website using only HTML and CSS, focusing on responsive design across devices.",
+                "Responsive Practice Website – Built a static website using only HTML and CSS, focusing on responsive design across devices.",
             tech: ["HTML", "CSS"],
             link: "https://harsh6468.github.io/swan-public-school",
             repo: "https://github.com/Harsh6468/swan-public-school",
@@ -100,7 +108,7 @@ export const PROFILE = {
         {
             title: "Get Today's weather",
             desc:
-            "Weather Report Website – A simple practice project that fetches and displays the weather report of any city.",
+                "Weather Report Website – A simple practice project that fetches and displays the weather report of any city.",
             tech: ["HTML", "CSS", "JavaScript"],
             link: "https://harsh6468.github.io/GetWeather",
             repo: "https://github.com/Harsh6468/GetWeather",
@@ -132,7 +140,7 @@ export const PROFILE = {
             marks: "9.0 CGPA"
         },
     ],
-    experiences:[
+    experiences: [
         {
             role: "Jr. Python Developer",
             company: "Ocius Technologies LLP",
